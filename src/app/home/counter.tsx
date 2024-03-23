de92dabf6e6e4ca55e4280768fc194007178410c
loewe <loewe.alivio@symph.co>
2024-03-23 20:21:14 +0800

    console logging hello world

--- src/app/home/counter.tsx
+++ src/app/home/counter.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    console.log("hello world");
 
     const increment = () => {
         setCount(count + 1);
