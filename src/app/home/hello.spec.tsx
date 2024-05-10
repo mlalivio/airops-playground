b076226b01428704caaf538aad2c0efbf8570750
Loewe <loewe.alivio@symph.co>
2024-05-10 16:56:35 +0800

    Committing a sample file

---
+++ src/app/home/hello.spec.tsx
@@ -0,0 +1,28 @@
+jsx
+// HelloWorld.test.js
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import HelloWorld from './HelloWorld';
+
+describe('HelloWorld Component', () => {
+
+  beforeEach(() => {
+    render(<HelloWorld />);
+  });
+
+  it('should be defined', () => {
+    const helloWorldComp = screen.getByText(/hello, world!/i);
+    expect(helloWorldComp).toBeDefined();
+  });
+
+  it('should display initial message "Hello, World!"', () => {
+    expect(screen.getByText(/hello, world!/i)).toBeDefined();
+  });
+
+  it('should change message to "You clicked the button!" when button is clicked', () => {
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(screen.getByText(/you clicked the button!/i)).toBeDefined();
+  });
+
+});
