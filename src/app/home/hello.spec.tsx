7dde712bf023b7ef09199d404a03063d95bef7c7
loewe <loewe.alivio@symph.co>
2024-05-10 16:58:27 +0800

    fix: corrected failing test case

--- src/app/home/hello.spec.tsx
+++ src/app/home/hello.spec.tsx
@@ -1,16 +1,13 @@
-jsx
-// HelloWorld.test.js
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import HelloWorld from './HelloWorld';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import HelloWorld from "./hello";
 
-describe('HelloWorld Component', () => {
-
+describe("HelloWorld Component", () => {
   beforeEach(() => {
     render(<HelloWorld />);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     const helloWorldComp = screen.getByText(/hello, world!/i);
     expect(helloWorldComp).toBeDefined();
   });
@@ -20,9 +17,8 @@ describe('HelloWorld Component', () => {
   });
 
   it('should change message to "You clicked the button!" when button is clicked', () => {
-    const button = screen.getByRole('button');
+    const button = screen.getByRole("button");
     fireEvent.click(button);
     expect(screen.getByText(/you clicked the button!/i)).toBeDefined();
   });
-
 });
