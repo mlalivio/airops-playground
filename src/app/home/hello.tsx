3a7f970b136536068a7199b147a993a1765c5254
loewe <loewe.alivio@symph.co>
2024-05-10 16:56:10 +0800

    chore: added console log

--- src/app/home/hello.tsx
+++ src/app/home/hello.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function HelloWorld() {
   const [message, setMessage] = useState("Hello, World!");
+  console.log("hello");
 
   const handleClick = () => {
     setMessage("You clicked the button!");
